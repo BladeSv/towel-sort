a64b57d0d20fdf7d676e64f2138d16c04feaad15
Mikail Mitrahovich <bladesvet@gmail.com>
2021-03-13 19:07:37 +0300

    add method implimentation

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    if (!matrix || !matrix.length) {return [];}
+  const comparator=[(a,b)=>a-b,(a,b)=>b-a];
+  matrix.forEach((el,index)=>el.sort(comparator[index%2]));
+  return matrix.flat();
 }
